115ef90556fb609747fbf5b90dab03b44c8d79de
Jacob Roberts <jacob@churchofthehighlands.com>
2018-03-09 11:08:39 -0600

    Fixes matchers_spec

--- spec/about_matchers_spec.rb
+++ spec/about_matchers_spec.rb
@@ -3,48 +3,48 @@ require 'spec_helper'
 describe "Matchers" do
 
   it "should allow matching with equality operator" do
-    true.should == false
+    expect(true == true)
   end
 
   it "should allow matching with built in matchers" do
-    true.should be_false
+    expect(true).to be_truthy
   end
 
   it "should allow matching with built in matchers and a message" do
-    true.should_not be_true, "This should be false -- Please fix this"
+    expect(true).to be_truthy, "This should be false -- Please fix this"
   end
 
   it "should allow matching of variables" do
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
-    actual_value.should == expected_value
+    expect(actual_value == expected_value)
   end
 
   it "should allow matching of variables using a better way" do
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
-    actual_value.should eq(expected_value)
+    expect(actual_value).to eq(expected_value)
   end
 
   it "should allow matching of values using equality" do
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
-    actual_value.should eql(expected_value)
+    expect(actual_value).to eql(expected_value)
   end
 
   # We should all have our own unique identity
   it "should allow matching of identity using equality " do
     expected_value = "I should be something else"
-    actual_value = expected_value
+    actual_value = expected_value + "."
 
-    actual_value.should_not equal(expected_value)
+    expect(actual_value).not_to equal(expected_value)
   end
 
   it "should allow matching near values" do
-    2.should be_within(3).of(_n_)
+    expect(999997).to be_within(3).of(_n_)
   end
 
   #be_nil            - nil
