77e15d5c5abcb69d37e195ea78f63e3b785edffc
Jacob Roberts <jacob@churchofthehighlands.com>
2018-03-09 11:46:33 -0600

    Fixes objects_spec

--- spec/about_objects_spec.rb
+++ spec/about_objects_spec.rb
@@ -3,45 +3,45 @@ require 'spec_helper'
 describe "Objects" do
 
   it "should show that everything is an object" do
-    1.is_a?(Object).should == __
-    1.5.is_a?(Object).should == __
-    "string".is_a?(Object).should == __
-    nil.is_a?(Object).should == __
-    Object.is_a?(Object).should == __
+    expect(1.is_a?(Object) == Object)
+    expect(1.5.is_a?(Object) == Object)
+    expect("string".is_a?(Object) == Object)
+    expect(nil.is_a?(Object) == Object)
+    expect(Object.is_a?(Object) == Object)
   end
 
   it "should convert objects to strings" do
-    123.to_s.should eql __
-    nil.to_s.should eql __
+    expect(123.to_s).to eql "123"
+    expect(nil.to_s).to eql ""
   end
 
   it "should be able to be inspected" do
-    123.inspect.should eql __
-    nil.inspect.should eql __
+    expect(123.inspect).to eql "123"
+    expect(nil.inspect).to eql "nil"
   end
 
   it "should show that objects have ids" do
     obj = Object.new
-    obj.object_id.class.should == __
+    expect(obj.object_id.class == "Integer")
   end
 
   it "should create objects with different ids" do
     obj = Object.new
     another_obj = Object.new
-    (obj.object_id != another_obj.object_id).should == __
+    expect(obj.object_id != another_obj.object_id).to eql true
   end
 
   it "should have the same object id for some system objects" do
-    false.object_id.should == __
-    true.object_id.should == __
-    nil.object_id.should == __
+    expect(false.object_id).to eql 0
+    expect(true.object_id).to eql 20
+    expect(nil.object_id).to eql 8
   end
 
   it "should have fixed ids for small integers" do
-    0.object_id.should == __
-    1.object_id.should == __
-    2.object_id.should == __
-    100.object_id.should == __
+    expect(0.object_id).to eql 1
+    expect(1.object_id).to eql 3
+    expect(2.object_id).to eql 5
+    expect(100.object_id).to eql 201
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
@@ -51,7 +51,7 @@ describe "Objects" do
     obj = Object.new
     copy = obj.clone
 
-    (obj != copy).should == __
-    (obj.object_id != copy.object_id).should == __
+    expect(obj != copy).to eql true
+    expect(obj.object_id != copy.object_id).to eql true
   end
 end
