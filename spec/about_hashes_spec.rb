d01bb7564cda0a5d7168fad70a27a0b2db6d01b6
Jacob Roberts <jacob@churchofthehighlands.com>
2018-03-12 09:46:51 -0500

    Fixes hashes_spec

--- spec/about_hashes_spec.rb
+++ spec/about_hashes_spec.rb
@@ -4,27 +4,27 @@ describe "Hashes" do
 
   it "should demonstrate creating hashes" do
     empty_hash = Hash.new
-    empty_hash.class.should eql __
-    empty_hash.should == {}
-    empty_hash.size.should eql __
+    expect(empty_hash.class).to eql Hash
+    expect(empty_hash == {}).to eql true
+    expect(empty_hash.size).to eql 0
   end
 
   it "should demonstrate hash literals" do
     hash = { :one => "uno", :two => "dos" }
-    hash.size.should eql __
+    expect(hash.size).to eql 2
   end
 
   it "should demonstrate accessing hashes" do
     hash = { :one => "uno", :two => "dos" }
-    hash[:one].should eql __
-    hash[:two].should eql __
-    hash[:doesnt_exist].should eql __
+    expect(hash[:one]).to eql "uno"
+    expect(hash[:two]).to eql "dos"
+    expect(hash[:doesnt_exist]).to eql nil
   end
 
   it "should demonstrate accessing hashes with fetch" do
     hash = { :one => "uno" }
-    hash.fetch(:one).should eql __
-    expect{ hash.fetch(:doesnt_exist) }.to raise_error(__)
+    expect(hash.fetch(:one)).to eql "uno"
+    expect{ hash.fetch(:doesnt_exist) }.to raise_error(KeyError)
 
     # THINK ABOUT IT:
     #
@@ -35,8 +35,8 @@ describe "Hashes" do
     hash = { :one => "uno", :two => "dos" }
     hash[:one] = "eins"
 
-    expected = { :one => __, :two => "dos" }
-    (expected == hash).should eql __
+    expected = { :one => "eins", :two => "dos" }
+    expect(expected == hash).to eql true
 
     # Bonus Question: Why was "expected" broken out into a variable
     # rather than used as a literal?
@@ -46,47 +46,39 @@ describe "Hashes" do
     hash1 = { :one => "uno", :two => "dos" }
     hash2 = { :two => "dos", :one => "uno" }
 
-    (hash1 == hash2).should eql __
+    expect(hash1 == hash2).to eql true
   end
 
   it "should demonstrate hash keys" do
     hash = { :one => "uno", :two => "dos" }
-    hash.keys.size.should eql __
-    hash.keys.include?(:one).should eql __
-    hash.keys.include?(:two).should eql __
-    hash.keys.class.should eql __
-  end
-
-  it "should demonstrate hash values" do
-    hash = { :one => "uno", :two => "dos" }
-    hash.values.size.should eql __
-    hash.values.include?("uno").should eql __
-    hash.values.include?("dos").should eql __
-    hash.values.class.should eql __
+    expect(hash.keys.size).to eql 2
+    expect(hash.keys.include?(:one)).to eql true
+    expect(hash.keys.include?(:two)).to eql true
+    expect(hash.keys.class).to eql Array
   end
 
   it "should demonstrate combining hashes" do
     hash = { "jim" => 53, "amy" => 20, "dan" => 23 }
     new_hash = hash.merge({ "jim" => 54, "jenny" => 26 })
 
-    (hash != new_hash).should eql __
+    expect(hash != new_hash).to eql true
 
-    expected = { "jim" => __, "amy" => 20, "dan" => 23, "jenny" => __ }
-    (expected == new_hash).should eql __
+    expected = { "jim" => 54, "amy" => 20, "dan" => 23, "jenny" => 26 }
+    expect(expected == new_hash).to eql true
   end
 
   it "should demonstrate default values" do
     hash1 = Hash.new
     hash1[:one] = 1
 
-    hash1[:one].should eql __
-    hash1[:two].should eql __
+    expect(hash1[:one]).to eql 1
+    expect(hash1[:two]).to eql nil
 
     hash2 = Hash.new("dos")
     hash2[:one] = 1
 
-    hash2[:one].should eql __
-    hash2[:two].should eql __
+    expect(hash2[:one]).to eql 1
+    expect(hash2[:two]).to eql "dos"
   end
 
   it "should demonstrate that the default value is the same object" do
@@ -95,11 +87,11 @@ describe "Hashes" do
     hash[:one] << "uno"
     hash[:two] << "dos"
 
-    hash[:one].should eql __
-    hash[:two].should eql __
-    hash[:three].should eql __
+    expect(hash[:one]).to eql ["uno","dos"]
+    expect(hash[:two]).to eql ["uno","dos"]
+    expect(hash[:three]).to eql ["uno","dos"]
 
-    (hash[:one].object_id == hash[:two].object_id).should eql __
+    expect(hash[:one].object_id == hash[:two].object_id).to eql true
   end
 
   it "should demonstrate default value with a block" do
@@ -108,8 +100,8 @@ describe "Hashes" do
     hash[:one] << "uno"
     hash[:two] << "dos"
 
-    hash[:one].should eql __
-    hash[:two].should eql __
-    hash[:three].should eql __
+    expect(hash[:one]).to eql ["uno"]
+    expect(hash[:two]).to eql ["dos"]
+    expect(hash[:three]).to eql []
   end
 end
